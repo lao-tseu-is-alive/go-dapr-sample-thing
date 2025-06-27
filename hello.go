dd136a79ad4c39e9c41b41136ce7a7637063ec31
cgil <lao.tseu.is.alive@gmail.com>
2025-06-27 16:58:31 +0200

    displaying dapr version

--- hello.go
+++ hello.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	dapr "github.com/dapr/go-sdk/client"
 	"os"
 )
 
 func main() {
+	fmt.Printf("Starting hello world from Darp app")
 	err := os.Setenv("DAPR_CLIENT_TIMEOUT_SECONDS", "3")
 	if err != nil {
-		fmt.Printf("error setting DAPR_CLIENT_TIMEOUT_SECONDS environment variable: %s", err)
-		return
+		fmt.Printf("💥💥 error setting DAPR_CLIENT_TIMEOUT_SECONDS environment variable: %s", err)
+		os.Exit(1)
 	}
 	client, err := dapr.NewClient()
 	if err != nil {
-		panic(err)
+		fmt.Printf("💥💥 error creating Dapr client: %s", err)
+		os.Exit(1)
 	}
 	defer client.Close()
+
+	// Get Dapr metadata to retrieve the version
+	metadata, err := client.GetMetadata(context.Background())
+	if err != nil {
+		fmt.Printf("💥💥 error getting Dapr metadata: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Print the entire metadata response to inspect available fields
+	fmt.Printf("Dapr Metadata: %+v\n", metadata)
+	// Print the Dapr runtime version
+	fmt.Printf("Dapr Runtime Version: %s\n", metadata.ExtendedMetadata["daprRuntimeVersion"])
 }
--- scripts/runDaprApp.sh
+++ scripts/runDaprApp.sh
@@ -5,9 +5,14 @@ if [ $# -eq 1 ]; then
 else
   APP_NAME=main
 fi
+# check if the file exist
+if [ ! -f $APP_NAME ]; then
+  echo "💥💥💥 the file ${APP_NAME} does not exist"
+  exit 1
+fi
 # get the name of the go file to run as first argument
 
 dapr run --app-id example-service \
          --app-protocol grpc \
          --app-port 50001 \
-         go run "${APP_NAME}.go"
+         go run "${APP_NAME}"
